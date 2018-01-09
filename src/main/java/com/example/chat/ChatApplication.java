5cd5348b33d8a67b1d2ac7b7d9fcd266515ef36a
caizheng <ytcz1992@163.com>
2018-01-09 14:33:22 +0800

    初始化

--- src/main/java/com/example/chat/ChatApplication.java
+++ src/main/java/com/example/chat/ChatApplication.java
@@ -2,10 +2,29 @@ package com.example.chat;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
 
 @SpringBootApplication
+@RestController
+@RequestMapping(value = "/User")
 public class ChatApplication {
 
+	@RequestMapping("/index")
+
+	public Map<String, Object> home() {
+		Map<String, Object> model = new HashMap<String, Object>();
+		model.put("id", UUID.randomUUID().toString());
+		model.put("content", "Hello World");
+		return model;
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(ChatApplication.class, args);
 	}
--- src/main/resources/static/js/main/MainController.js
+++ src/main/resources/static/js/main/MainController.js
@@ -9,8 +9,8 @@
  * # Created by zhongkewen on 2015/6/9.
  */
 QY.CONTROLLERS
-    .controller('MainController', ["$scope", "$state", "$log",
-        function ($scope, $state, $log) {
+    .controller('MainController', ["$scope", "$state", "$log","$http",
+        function ($scope, $state, $log,$http) {
             "use strict";
             //
             // //初始化变量和ready回调函数数组
@@ -34,18 +34,50 @@ QY.CONTROLLERS
             },
                 {
                     "select": "text",
-                    "desc": "请选择年龄",
-                    "option": [{"label": "0-6岁", "value": "0"}, {"label": "7-12", "value": "1"}, {"label": "16-35", "value": "2"}, {"label": "其他", "value": "3"}],
+                    "desc": "请问有什么不适症状？",
+                    "label":"请输入症状，例如发烧、咳嗽等",
+                    "content":"",
+                    "isShow":false
+
+                }, {
+                    "select": "text",
+                    "desc": "请问有什么不适症状？",
+                    "label":"请输入症状，例如发sssssssss",
+                    "content":"",
                     "isShow":false
 
                 }];
 
             $scope.anwser=[];
+            $scope.text={"content":""};
 
             $scope.change=function (outerIndex,innerIndex) {
                 $scope.data[outerIndex].isShow=true;
                 $scope.anwser[outerIndex]=$scope.data[outerIndex].option[innerIndex].label;
 
+            };
+            $scope.conform=function (outerIndex,innerIndex) {
+                $scope.data[outerIndex].isShow=true;
+                $scope.anwser[outerIndex]=$scope.data[outerIndex].content;
+                $http.get('/User/index').success(function (data) {
+                    if (data) {
+                        var s ={
+                            "select": "last",
+                            "desc": "请选择性别",
+                            "option": [{"label": "男", "value": "0"}, {"label": "女", "value": "1"}],
+                            "isShow":false
+
+                        }
+                        $scope.data.push(s);
+
+                    }
+                    else {
+
+                    }
+
+                }).error(function () {
+
+                });
             };
             $scope.modify=function (outerIndex) {
                 $scope.data[outerIndex].isShow=false;
